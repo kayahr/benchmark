826be57e5a061390719ef131109601ddb42b64a4
Klaus Reimer <k@ailis.de>
2025-06-02 20:31:00 +0200

    Update vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -11,27 +11,44 @@ export default defineConfig(
         env: {
             NODE_OPTIONS: `${process.env.NODE_OPTIONS ?? ""} --expose-gc`
         },
-        browser: {
-            provider: "playwright",
-            headless: true,
-            screenshotFailures: false,
-            instances: [
-                {
-                    browser: "chromium",
-                    launch: {
-                        args: [
-                            "--js-flags=--expose-gc"
-                        ]
-                    }
-                }
-            ]
-        },
         coverage: {
             enabled: true,
             provider: "istanbul",
             reporter: [ "text-summary", "json", "lcov", "clover", "cobertura", "html" ],
             reportsDirectory: "lib/test/coverage",
             include: [ "src/main/**/*.ts" ]
-        }
+        },
+        projects: [
+            {
+                extends: true,
+                test: {
+                    name: "Node",
+                    exclude: [ "**/*.browser.test.*" ]
+                }
+            },
+            {
+                extends: true,
+                test: {
+                    name: "Browser",
+                    exclude: [ "**/*.node.test.*" ],
+                    browser: {
+                        enabled: true,
+                        provider: "playwright",
+                        headless: true,
+                        screenshotFailures: false,
+                        instances: [
+                            {
+                                browser: "chromium",
+                                launch: {
+                                    args: [
+                                        "--js-flags=--expose-gc"
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        ]
     }
 });
